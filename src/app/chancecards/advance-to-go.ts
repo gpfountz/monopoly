b97a3a0be6146387217f6efd61c0ea359e38ecf6
Greg Pfountz <greg@GregsMBPRetina.pfountz.home>
2017-08-21 23:24:48 -0400

    release 5 incomplete

--- src/app/chancecards/advance-to-go.ts
+++ src/app/chancecards/advance-to-go.ts
@@ -1,2 +1,13 @@
-export class AdvanceToGo {
+import { Chancecard } from "app/chancecards/chancecard";
+import { Board } from "app/board";
+import { PlayerToken } from "app/player-tokens.enum";
+
+export class AdvanceToGo implements Chancecard {
+    readonly desc: string;
+    constructor(desc: string) {
+        this.desc = desc;
+    }
+    draw(board: Board, player: PlayerToken) {
+        
+    }
 }
--- src/app/chancecards/chancecard.ts
+++ src/app/chancecards/chancecard.ts
@@ -1,2 +1,7 @@
+import { Board } from "app/board";
+import { PlayerToken } from "app/player-tokens.enum";
+
 export interface Chancecard {
+    desc: string;
+    draw(board: Board, player: PlayerToken);
 }
