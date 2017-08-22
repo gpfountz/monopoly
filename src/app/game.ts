f427e13267888bf27ab8df91261a67bb895ec61d
Greg Pfountz <greg@GregsMBPRetina.pfountz.home>
2017-08-21 23:24:24 -0400

    release 5 incomplete

--- src/app/game.ts
+++ src/app/game.ts
@@ -8,45 +8,24 @@ export class Game {
     public static PLAYER_MIN_MAX_ERROR: string = 'must have 2 to 8 players to play Monopoly';
 
     private board: Board;
-    private players: Player[];
     private dice: Dice;
 
     constructor(playerTokens: PlayerToken[]) {
         if (playerTokens.length < 2 || playerTokens.length > 8) {
             throw new Error(Game.PLAYER_MIN_MAX_ERROR);
         }
-        this.board = new Board();
         this.dice = new RealDice();
-        this.players = new Array();
+        let players = new Array();
         for (let playerToken of playerTokens) {
-            this.players.push(new Player(playerToken));
+            players.push(new Player(playerToken));
         }
-        this.shuffle(this.players);
+        this.shuffle(players);
+        this.board = new Board(players);
     }
 
     /** get this number of players in the game */
     public getNumberPlayers(): number {
-        return this.players.length;
-    }
-
-    /** get the player for a specific player token */
-    public getPlayer(playerToken: PlayerToken) {
-        for (let player of this.players) {
-            if (player.getToken() === playerToken) {
-                return player;
-            }
-        }
-        return undefined;
-    }
-
-    /** get the order of play for a specific player.  result is 0 based, 0 being the first in order */
-    public getPlayerOrder(playerToken: PlayerToken): number {
-        for (let i = 0; i < this.players.length; i++) {
-            if (this.players[i].getToken() === playerToken) {
-                return i;
-            }
-        }
-        return -1;
+        return this.board.getPlayers().length;
     }
 
     /** play a number of rounds.  each player moves once per round. */
@@ -58,7 +37,7 @@ export class Game {
 
     /** play 1 round.  each player moves once. */
     public playRound() {
-        for(let player of this.players) {
+        for(let player of this.board.getPlayers()) {
             player.move(this.board, this.dice);
         }
     }
